4c8bf376fb211ec94cfbfd3062135afe477fca1c
Zhirui Dai <daizhirui@hotmail.com>
2025-11-19 21:37:55 -0800

    add libzip, fix python build

---
+++ cmake/erl_config_libzip.cmake
@@ -0,0 +1,13 @@
+macro(erl_config_libzip)
+    option(ERL_USE_LIBZIP "Use libzip" ON)
+    if (ERL_USE_LIBZIP)
+        erl_find_package(
+            PACKAGE libzip
+            REQUIRED
+            COMMANDS ARCH_LINUX "try `sudo pacman -S libzip`"
+            COMMANDS UBUNTU_LINUX "try `sudo apt-get install libzip-dev`"
+        )
+
+        add_compile_definitions(ERL_USE_LIBZIP)
+    endif ()
+endmacro()
